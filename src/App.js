75df342e8cc54ab99763235154e80dad7c5438cc
Tatu2002vz <91172883+Tatu2002vz@users.noreply.github.com>
2024-06-07 08:32:21 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -47,7 +47,6 @@ function App() {
             );
           })
           }
-          <Route path="*" element={<Navigate to={path.HOME}/>}/>
         </Routes>
         <ToastContainer />
       </div>
